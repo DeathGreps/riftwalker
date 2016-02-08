6cd14020ff92aaafb4f458e35bec5f7fb882dc4d
kevdez <kernan373@gmail.com>
2016-02-08 11:28:48 -0800

    Added item set images

--- dev/js/itemSetController.js
+++ dev/js/itemSetController.js
@@ -2,6 +2,38 @@ var app = angular.module('riftwalkerApp');
 
 app.controller('ItemSetController', [ 'DataService', '$scope', '$http',function( DataService, $scope, $http ) {
 
+  var getItems = function() {
+      $http({
+          method: 'GET',
+          dataType: 'jsonp',
+          // url: '/api/staticdata/' + DataService.getRegion().host + '/' + DataService.getRegion().region
+          url: 'https://global.api.pvp.net/api/lol/static-data/' + DataService.getRegion().region.toLowerCase() + '/v1.2/item?itemListData=all&api_key='
+      }).then(function successCallback(response) {
+        $scope.items = response.data;
+        $scope.itemData = response.data.data;
+        $scope.itemBasic = response.data.basic;
+        $scope.itemTree = response.data.tree;
+        $scope.itemGroups = response.data.groups;
+
+
+      }, function errorCallback(response) {
+          console.log(response);
+      });
+  };
+
+  $scope.getItemImage = function(imageFullName) {
+      return itemImageUrl + imageFullName;
+  };
+
+  var itemImageUrl = "http://ddragon.leagueoflegends.com/cdn/6.2.1/img/item/";
+  getItems();
+  $scope.selectedRegion = DataService.getRegion();
+
+  $scope.items = getItems();
+  $scope.itemData;
+  $scope.itemBasic;
+  $scope.itemTree;
+  $scope.itemGroups;
 }])
 .directive('itemsetForm', function () {
   return {
