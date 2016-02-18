1ca96ebda68a5178b442fe3c2ab49aba21314291
kevdez <kernan373@gmail.com>
2016-02-18 01:40:44 -0800

    Item tag filtering works! Redesigned step 2 and took out md-cards

--- dev/js/itemSetController.js
+++ dev/js/itemSetController.js
@@ -1,6 +1,6 @@
 var app = angular.module('riftwalkerApp');
 
-app.controller('ItemSetController', [ 'DataService', '$scope', '$http',function( DataService, $scope, $http ) {
+app.controller('ItemSetController', [ 'DataService', '$scope', '$http', '$filter', function( DataService, $scope, $http, $filter) {
 
   var getItems = function() {
       $http({
@@ -13,6 +13,7 @@ app.controller('ItemSetController', [ 'DataService', '$scope', '$http',function(
       }).then(function successCallback(response) {
         $scope.items = response.data;
         $scope.itemData = response.data.data;
+        $scope.filteredItemData = response.data.data;
         $scope.itemBasic = response.data.basic;
         $scope.itemTree = response.data.tree;
         $scope.itemGroups = response.data.groups;
@@ -23,13 +24,46 @@ app.controller('ItemSetController', [ 'DataService', '$scope', '$http',function(
       });
   };
 
-  getItems();
-
   $scope.getItemImage = function(imageFullName) {
       return itemImageUrl + imageFullName;
   };
 
   var itemImageUrl = "http://ddragon.leagueoflegends.com/cdn/6.3.1/img/item/";
+
+  $scope.treeTagsUpdated = function(TagToDelete) {
+    if(TagToDelete) {
+      $scope.treeTagsSelected.splice($scope.treeTagsSelected.indexOf(TagToDelete), 1);
+    }
+    console.log($scope.treeTagsSelected);
+    if($scope.treeTagsSelected.length == 0) {
+      $scope.filteredItemData = $scope.itemData;
+    } else {
+      $scope.filteredItemData = {};
+      for(var itemId in $scope.itemData) {
+        var itemObject = $scope.itemData[itemId]
+        var includeThisItem = true;
+        if(typeof(itemObject.tags) == 'undefined')
+          continue;
+        for(var i in $scope.treeTagsSelected) {
+          var tempTag = $filter('lowercase')($scope.treeTagsSelected[i]);
+          var tagIsInItem = false;
+          for(var j in itemObject.tags) {
+            if (tempTag == ($filter('lowercase')(itemObject.tags[j]))) {
+              tagIsInItem = true;
+              break;
+            }
+          }
+          if(!tagIsInItem) {
+            includeThisItem = false;
+            break;
+          }
+        }
+        if(includeThisItem) {
+          $scope.filteredItemData[itemId] = $scope.itemData[itemId];
+        }
+      }
+    }
+  }
 
   $scope.selectTreeTag = function(treeHeader) {
     if ($scope.treeTagsSelected.indexOf(treeHeader) >= 0) {
@@ -47,6 +81,7 @@ app.controller('ItemSetController', [ 'DataService', '$scope', '$http',function(
 
   $scope.items = getItems();
   $scope.itemData;
+  $scope.filteredItemData;
   $scope.itemBasic;
   $scope.itemTree;
   $scope.itemGroups;
